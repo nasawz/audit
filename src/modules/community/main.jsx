56af26fe398a1195fc8b4763258f63188c33bf0e
王喆 <nasa127@gmail.com>
2016-09-19 15:48:58 +0800

    路由保持一致性

--- src/modules/community/main.jsx
+++ src/modules/community/main.jsx
@@ -8,8 +8,6 @@ import { Provider } from 'react-redux'
 import store from './store'
 import Routes from './routes'
 
-import {navigate} from 'react-mini-router'
-
 const App = React.createFactory(Routes)
 
 import '../../style/index.less'
@@ -22,8 +20,7 @@ require('safe')(React, {
 
 ReactDOM.render(
     <Provider store={store()}>
-        {App({ root:'/message/hello' })}
+        {App({ root:'' })}
     </Provider>,
     document.getElementById('container')
 )
-navigate('/message/hello')
--- src/modules/community/views/home.jsx
+++ src/modules/community/views/home.jsx
@@ -4,10 +4,15 @@
 import React from 'react'
 import PureRenderDecoratorMixin from '../../common/pure-render-decorator-mixin.js'
 
+import {navigate} from 'react-mini-router'
+
 const Home = React.createClass({
     mixins:[PureRenderDecoratorMixin],
     componentDidMount() {
         this.props.actions.sayHello('home')
+        setTimeout(function () {
+            navigate('/message/hello')
+        }, 1000)
     },
     render () {
         return (
