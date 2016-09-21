491f61a395c9d682ee1af34b157299153b8fecdc
吴丹丹 <danw@kailuzhe.cn>
2016-09-21 16:02:01 +0800

    柱状图控件

--- src/modules/audit/views/overview.jsx
+++ src/modules/audit/views/overview.jsx
@@ -9,6 +9,7 @@ import NavigateMixin from '../../common/navigate-mixin.js'
 import HeaderBar from 'audit/bar/header-bar.jsx'
 import SideNavBar from 'audit/bar/side-nav-bar.jsx'
 import ContentWapper from 'audit/layout/content-wapper.jsx'
+import StackedBarChart from 'audit/charts/stacked-bar-chart.jsx'
 
 const Overview = React.createClass({
     mixins:[PureRenderDecoratorMixin,NavigateMixin],
@@ -22,7 +23,8 @@ const Overview = React.createClass({
                 <div>
                     <SideNavBar></SideNavBar>
                     <ContentWapper>
-                        {this.props.name} say hello
+                        <StackedBarChart />
+                       
                     </ContentWapper>
                 </div>
             </div>
