682c0452b28035910fac89424dbaa35a3436ddb5
李继红 <707230595@qq.com>
2016-09-22 01:08:43 +0800

    fix

--- src/modules/audit/views/overview.jsx
+++ src/modules/audit/views/overview.jsx
@@ -17,6 +17,8 @@ import TwoLevelPieChart from 'audit/charts/two-level-pie-chart.jsx'
 
 import navConf from '../nav-config.js'
 
+import AlertChart from '../../../components/audit/alert/alertChart.jsx'
+
 const Overview = React.createClass({
     mixins:[PureRenderDecoratorMixin,NavigateMixin],
     componentDidMount() {
@@ -24,7 +26,10 @@ const Overview = React.createClass({
     },
     getInitialState() {
         return {
-            idx: 1
+            idx: 1,
+            showProgressAlert: false,
+            showCapitalAlert: false,
+            showMarketAlert: false
         }
     },
     handleClickPrev() {
@@ -34,16 +39,38 @@ const Overview = React.createClass({
         })
     },
     handleClickNext() {
-        console.log(this.state.idx);
         if (this.state.idx == 2) return
         this.setState({
             idx: 2
         })
     },
+    showAlert(e){
+        let type = e.currentTarget.getAttribute('data-type')
+        if (type == 'progress') {
+            this.setState({
+                showProgressAlert:true
+            });
+        } else if (type == 'capital') {
+            this.setState({
+                showCapitalAlert:true
+            });
+        } else {
+            this.setState({
+                showMarketAlert:true
+            });
+        }
+    },
+    closeAlert(){
+        this.setState({
+            showProgressAlert:false,
+            showCapitalAlert:false,
+            showMarketAlert:false
+        });
+    },
     renderProgress() {
         if (this.state.idx == 1) {
             return (
-                <div>
+                <div data-type="progress" onClick={this.showAlert}>
                     <div className="overviewProgressBox">
                         <TwoLevelPieChart width={200} height={200} state="s1" />
                     </div>
@@ -107,7 +134,7 @@ const Overview = React.createClass({
                                 <div className="columns">
                                     <div className="column">
                                         <div>
-                                            <a href="javascript:void(0)" title="人工成本及劳务费">人工成本及劳务费</a>
+                                            <a href="javascript:void(0)" title="人工成本（劳务费）主要包括：职工工资总额、社会保险费用、职工福利费用、职工教育经费、劳动保护费用、职工住房费用和其他人工成本支出。其中，职工工资总额是人工成本的主要组成部分。平均人工成本=人工成本总额/同期人数。人工成本比重=人工成本总额/同期总成本*100%。劳动分配率=人工成本总额/同期增加值*100%。人工成本亦称“劳动成本”。指企业在生产经营中由投入劳动力要素所发生的一切费用，包括企业支付给职工的工资性报酬和福利性供给，是企业总成本的组成部分。">人工成本及劳务费</a>
                                             <ul>
                                                 <li>人工成本</li>
                                                 <li>劳务费</li>
@@ -133,7 +160,7 @@ const Overview = React.createClass({
                                         </div>
                                     </div>
                                     <div className="column">
-                                        <div>
+                                        <div data-type="capital" onClick={this.showAlert}>
                                             <a href="javascript:void(0)" title="资金管理">资金管理</a>
                                             <ul>
                                                 <li>账户管理</li>
@@ -142,8 +169,8 @@ const Overview = React.createClass({
                                         </div>
                                     </div>
                                     <div className="column">
-                                        <div>
-                                            <a href="javascript:void(0)" title="业务合规性">业务合规性</a>
+                                        <div data-type="market" onClick={this.showAlert}>
+                                            <a href="javascript:void(0)" title="在合规性审计中，审计人员应确定公司是否遵循了现行法律和法规以及专业和行业标准或合同责任的要求，即被审计单位是否遵循了特定的程序、规则或条例。例如，确定会计人员是否遵循了财务主管规定的手续，检查工资率是否符合工资法规定的最低限额，或者审查与银行签订的合同，以确信被审计单位遵守了法定要求。对审计人员来讲，进行合规性审计的第一步是确定管理层是否有一个识别现行政策、程序、标准、法律以及法规的制度；然后，审计人员应评估控制是否得到了恰当的应用或遵循；最后，该审计应得到公司是否合规的结论">业务合规性</a>
                                             <ul>
                                                 <li>大众市场</li>
                                                 <li>终端营销</li>
@@ -224,6 +251,61 @@ const Overview = React.createClass({
                                 </div>
                             </div>
                         </div>
+                        <AlertChart show={this.state.showProgressAlert} close={this.closeAlert} title='省公司数据准备情况' top="1%" >
+                        	<img src={require('../../../img/progress-pic.jpg')} />
+                        </AlertChart>
+                        <AlertChart show={this.state.showCapitalAlert} close={this.closeAlert} title='资金管理' >
+                        		<h3>
+                        			定期归帐管理
+                                    <i className="qizi zise"></i>
+                        		</h3>
+                                <ul>
+                                    <li>审计说明：是否定期对账并对长期或大额未达账项跟踪管理。</li>
+                                    <li>审计发现：未发现数据异常</li>
+                                </ul>
+                                <h3>
+                                    资金归集是否违规
+                                    <i className="qizi zise"></i>
+                                </h3>
+                                <ul>
+                                    <li>审计说明：是否在规定范围内开设银行存款账户，按照总部要求归集相应的资金</li>
+                                    <li>审计发现：未发现数据异常</li>
+                                </ul>
+                                <h3>
+                                    在非合作银行违规存款
+                                    <i className="qizi zise"></i>
+                                </h3>
+                                <ul>
+                                    <li>审计说明：是否在非总部规定的合作银行外保有定期存款及定期存款账户。</li>
+                                    <li>审计发现：未发现数据异常</li>
+                                </ul>
+                        </AlertChart>
+                        <AlertChart show={this.state.showCapitalAlert} close={this.closeAlert} title='资金管理' >
+                        		<h3>
+                        			定期归帐管理
+                                    <i className="qizi zise"></i>
+                        		</h3>
+                                <ul>
+                                    <li>审计说明：是否定期对账并对长期或大额未达账项跟踪管理。</li>
+                                    <li>审计发现：未发现数据异常</li>
+                                </ul>
+                                <h3>
+                                    资金归集是否违规
+                                    <i className="qizi zise"></i>
+                                </h3>
+                                <ul>
+                                    <li>审计说明：是否在规定范围内开设银行存款账户，按照总部要求归集相应的资金</li>
+                                    <li>审计发现：未发现数据异常</li>
+                                </ul>
+                                <h3>
+                                    在非合作银行违规存款
+                                    <i className="qizi zise"></i>
+                                </h3>
+                                <ul>
+                                    <li>审计说明：是否在非总部规定的合作银行外保有定期存款及定期存款账户。</li>
+                                    <li>审计发现：未发现数据异常</li>
+                                </ul>
+                        </AlertChart>
                     </ContentWapper>
                 </div>
             </div>
