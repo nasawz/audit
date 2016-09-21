bac12d3c842f6a3a205cbfd3a26930e1e80b6bb6
王喆 <nasa127@gmail.com>
2016-09-22 00:48:32 +0800

    fix

--- src/modules/audit/views/creategroup.jsx
+++ src/modules/audit/views/creategroup.jsx
@@ -23,7 +23,44 @@ const CreateGroup = React.createClass({
                         <SideNavBarItem icon="creategroup" path={navConf.leftnav[6]}  selected  />
                     </SideNavBar>
                     <ContentWapper>
-                        <div className="columns clueBox"></div>
+                        <div className="clueBox creategroup">
+                            <h1 className="cardHeader">新建工作组</h1>
+                            <div className="columns">
+                                <div className="column is-11 is-offset-1">
+
+                                    <div className="container" style={{paddingTop:'80px'}}>
+                                        <div className="content">
+                                            <h3>组长：李华</h3>
+                                        </div>
+                                        <div className="control is-horizontal">
+                                          <div className="control-label">
+                                            <label className="label">选择部门</label>
+                                          </div>
+                                          <div className="control" style={{flexGrow:14}}>
+                                            <p className="control">
+                                              <input className="input" type="text" placeholder="Name" />
+                                            </p>
+                                          </div>
+                                        </div>
+                                        <div className="control is-horizontal">
+                                          <div className="control-label">
+                                            <label className="label">添加组员</label>
+                                          </div>
+                                          <div className="control" style={{flexGrow:14}}>
+
+                                          </div>
+                                        </div>
+                                        <div className='flex-center'>
+                                            <div>
+                                                <a className="button is-dark" style={{width:'120px',marginRight:'20px'}}>取消</a>
+                                                <a className="button is-info" style={{width:'120px',marginRight:'20px'}}>下一步</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                     </ContentWapper>
                 </div>
             </div>
