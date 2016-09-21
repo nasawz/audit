6eaf449bcbfbc0ce84bb758030c5788e0019c1a4
王喆 <nasa127@gmail.com>
2016-09-22 01:03:09 +0800

    选择组件应用

--- src/modules/audit/views/creategroup.jsx
+++ src/modules/audit/views/creategroup.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react'
 
 import HeaderBar from 'audit/bar/header-bar.jsx'
@@ -6,8 +7,40 @@ import SideNavBarItem from 'audit/bar/side-nav-bar-item.jsx'
 import ContentWapper from 'audit/layout/content-wapper.jsx'
 
 import navConf from '../nav-config.js'
+import { Select,Transfer } from 'antd'
 
 const CreateGroup = React.createClass({
+getInitialState() {
+    return {
+      mockData: [],
+      targetKeys: [],
+    };
+  },
+  componentDidMount() {
+    this.getMock();
+  },
+  getMock() {
+    const targetKeys = [];
+    const mockData = [];
+    let names = ['小明','小王','小李','小赵','小红','小张']
+    for (let i = 0; i < 5; i++) {
+      const data = {
+        key: i,
+        title: names[i],
+        description: '',
+        chosen: false,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({ mockData, targetKeys });
+  },
+  handleChange(targetKeys, direction, moveKeys) {
+    console.log(targetKeys, direction, moveKeys);
+    this.setState({ targetKeys });
+  },
     render () {
         return (
             <div>
@@ -38,16 +71,32 @@ const CreateGroup = React.createClass({
                                           </div>
                                           <div className="control" style={{flexGrow:14}}>
                                             <p className="control">
-                                              <input className="input" type="text" placeholder="Name" />
+                                              <Select showSearch
+                                                style={{ width: 200 }}
+                                                placeholder="请选择部门"
+                                                optionFilterProp="children"
+                                                notFoundContent="无法找到"
+                                                >
+                                                <Option value="总部审核部">总部审核部</Option>
+                                                <Option value="北京移动">北京移动</Option>
+                                                <Option value="江西移动">江西移动</Option>
+                                                <Option value="安徽移动">安徽移动</Option>
+                                            </Select>
                                             </p>
                                           </div>
                                         </div>
-                                        <div className="control is-horizontal">
+                                        <div className="control is-horizontal" style={{marginBottom:'30px'}}>
                                           <div className="control-label">
                                             <label className="label">添加组员</label>
                                           </div>
                                           <div className="control" style={{flexGrow:14}}>
-
+                                                <Transfer
+                                                        titles={['备选人员','已选人员']}
+                                                        dataSource={this.state.mockData}
+                                                        targetKeys={this.state.targetKeys}
+                                                        onChange={this.handleChange}
+                                                        render={item => item.title}
+                                                      />
                                           </div>
                                         </div>
                                         <div className='flex-center'>
