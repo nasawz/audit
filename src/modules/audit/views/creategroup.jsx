8511b6cc5b58f6a0a4ee5531d91724fb49e112a1
王喆 <nasa127@gmail.com>
2016-09-21 23:30:04 +0800

    新增两个页面

--- src/modules/audit/views/creategroup.jsx
+++ src/modules/audit/views/creategroup.jsx
@@ -1,10 +1,4 @@
-/**
- * create by nasa.wang
- */
 import React from 'react'
-import PureRenderDecoratorMixin from '../../common/pure-render-decorator-mixin.js'
-
-import NavigateMixin from '../../common/navigate-mixin.js'
 
 import HeaderBar from 'audit/bar/header-bar.jsx'
 import SideNavBar from 'audit/bar/side-nav-bar.jsx'
@@ -13,11 +7,7 @@ import ContentWapper from 'audit/layout/content-wapper.jsx'
 
 import navConf from '../nav-config.js'
 
-const Brace = React.createClass({
-    mixins:[PureRenderDecoratorMixin,NavigateMixin],
-    componentDidMount() {
-        this.props.actions.sayHello('Brace')
-    },
+const CreateGroup = React.createClass({
     render () {
         return (
             <div>
@@ -25,14 +15,15 @@ const Brace = React.createClass({
                 <div>
                     <SideNavBar>
                         <SideNavBarItem icon="home" path={navConf.leftnav[0]} />
-                        <SideNavBarItem icon="overview" path={navConf.leftnav[1]} />
+                        <SideNavBarItem icon="overview" path={navConf.leftnav[1]}/>
                         <SideNavBarItem icon="circle" path={navConf.leftnav[2]} />
                         <SideNavBarItem icon="chart" path={navConf.leftnav[3]} />
                         <SideNavBarItem icon="clue" path={navConf.leftnav[4]} />
-                        <SideNavBarItem icon="brace" path={navConf.leftnav[5]} selected />
-                        <SideNavBarItem icon="creategroup" path={navConf.leftnav[6]}  />
+                        <SideNavBarItem icon="brace" path={navConf.leftnav[5]}  />
+                        <SideNavBarItem icon="creategroup" path={navConf.leftnav[6]}  selected  />
                     </SideNavBar>
                     <ContentWapper>
+                        <div className="columns clueBox"></div>
                     </ContentWapper>
                 </div>
             </div>
@@ -40,4 +31,4 @@ const Brace = React.createClass({
     }
 })
 
-export default Brace
+export default CreateGroup
