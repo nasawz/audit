7f6d45829e2f04df1187fbe90628a3aa83207170
王曼玉 <manyu.wang@hpe.com>
2016-09-21 23:03:49 +0800

    fix

--- src/components/audit/alert/alertChart.jsx
+++ src/components/audit/alert/alertChart.jsx
@@ -2,20 +2,7 @@ import React from 'react'
 import '../../../style/Alert.less'
 
 const AlertChart = React.createClass({
-    getInitialState(){
-        return {
-            cls:'content',
-            show:this.props.show,
-        }
-    },
-    changeClass(){
-        this.setState({
-            cls:'content content1'
-        })
-    },
-    goFinish(){
 
-    },
     closeAlert(e){
         e.stopPropagation();
         e.preventDefault();
@@ -29,7 +16,13 @@ const AlertChart = React.createClass({
         return (
             <div className="AlertBox" style={{minHeight:H,display:show}}>
                 <div className="box boxChart">
-                   
+                    <p className="boxTop">经营业绩考核指标和重点通报指标是否作假
+                        <i className="dele" data-target="1" onClick={this.closeAlert}></i>
+                    </p>
+                    <div className="alertCont" onClick={this.changeClass}>
+                        <h3>审计点说明</h3>
+                    </div>
+                    
                 </div>
                 <div className='alertBottom' onClick={this.closeAlert}>
 
